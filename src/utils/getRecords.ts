5b55787ca22f64818499b9b1a313036af6dcd565
demirarif <102763321+demirarif@users.noreply.github.com>
2025-11-07 14:35:22 +0300

    Add files via upload

---
+++ src/utils/getRecords.ts
@@ -0,0 +1,16 @@
+import { supabase } from './supabaseClient'
+
+export async function getRecords() {
+  try {
+    const { data, error } = await supabase
+      .from('records')
+      .select('*')
+      .order('created_at', { ascending: false })
+
+    if (error) throw error
+    return data || []
+  } catch (err) {
+    console.error('❌ Veriler çekilemedi:', err)
+    return []
+  }
+}
